8ea638374373b022b63ca1466af99bdf39906997
Samuel Hope <samuelhope805@gmail.com>
2025-05-30 17:48:42 +0300

    Initial commit

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: {
@@ -43,6 +43,71 @@ const productSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+module.exports = mongoose.model('Product', productSchema);*/
+
+const mongoose = require('mongoose');
+
+const detailSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  }
+});
+
+const specificationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  }
+});
+
+const productSchema = new mongoose.Schema({
+  mainCategory: {
+    type: String,
+    required: true
+  },
+  subCategory: {
+    type: String,
+    required: true
+  },
+  details: [detailSchema],
+  specifications: [specificationSchema],
+  batchNumber: {
+    type: String,
+    required: true
+  },
+  netContent: {
+    type: String,
+    required: true
+  },
+  notes: {
+    type: String,
+    default: ''
+  },
+  certifications: [{
+    type: String
+  }],
+  images: [{
+    type: String,
+    required: true
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
